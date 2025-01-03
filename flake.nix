8500797cb985c078596a3bcad3f83deb3a6f25b5
Terrence Asgar-Deen <48781331+pfhlump@users.noreply.github.com>
2025-01-03 10:42:44 -0700

    Formatting in vscode

--- flake.nix
+++ flake.nix
@@ -9,29 +9,35 @@
   };
 
   outputs = inputs@{ self, nix-darwin, nixpkgs, nix-homebrew }:
-  let
-    configuration = { pkgs, config, ... }: {
-      nixpkgs.config.allowUnfree = true;
-      homebrew = {
-        enable = true;
+    let
+      configuration = { pkgs, config, ... }: {
+        nixpkgs.config.allowUnfree = true;
+        homebrew = {
+          enable = true;
 
-	brews = [
-          "mas"
-	];
+          brews = [
+            "mas"
+            "bitwarden-cli"
+            "oh-my-posh"
+          ];
 
-	masApps = {
-           # "Amazon Prime Video" = 545519333;
+          casks = [
+            "alt-tab"
+            "visual-studio-code"
+          ];
+
+          masApps = {
+            # "Amazon Prime Video" = 545519333;
+          };
+
+          onActivation.cleanup = "zap";
+          onActivation.autoUpdate = true;
+          onActivation.upgrade = true;
         };
 
-	onActivation.cleanup = "zap";
-        onActivation.autoUpdate = true;
-        onActivation.upgrade = true;
-      };
-
-      # List packages installed in system profile. To search by name, run:
-      # $ nix-env -qaP | grep wget
-      environment.systemPackages =
-        [
+        # List packages installed in system profile. To search by name, run:
+        # $ nix-env -qaP | grep wget
+        environment.systemPackages = [
           pkgs.alacritty
           pkgs.ghostty
           pkgs.neovim
@@ -40,85 +46,100 @@
           pkgs.obsidian
           pkgs.github-cli
           pkgs.chezmoi
+          pkgs.aws-sso-cli
+          pkgs.awscli2
+          pkgs.azure-cli
+          # pkgs.bitwarden-cli
+          # pkgs.bitwarden-desktop
+          # There are a few versions of direnv to review.
+          pkgs.direnv
+          pkgs.fzf
+          pkgs.gopass
+          # pkgs.oh-my-posh
+          pkgs.gnused
+          # pkgs.vscode
+          # pkgs.vscode-fhs
+          pkgs.nixpkgs-fmt
         ];
 
-      fonts.packages = [
-        pkgs.nerd-fonts.jetbrains-mono
-        # (pkgs.nerdfonts.override { fonts = [ "JetBrainsMono" ]; })
-      ];
+        fonts.packages = [
+          pkgs.nerd-fonts.jetbrains-mono
+          # (pkgs.nerdfonts.override { fonts = [ "JetBrainsMono" ]; })
+        ];
 
-      system.activationScripts.applications.text = let
-        env = pkgs.buildEnv {
-          name = "system-applications";
-          paths = config.environment.systemPackages;
-          pathsToLink = "/Applications";
-        };
-      in
-        pkgs.lib.mkForce ''
-          # Set up applications.
-          echo "setting up /Applications..." >&2
-          rm -rf /Applications/Nix\ Apps
-          mkdir -p /Applications/Nix\ Apps
-          find ${env}/Applications -maxdepth 1 -type l -exec readlink '{}' + |
-          while read -r src; do
-            app_name=$(basename "$src")
-            echo "Making alias $src" >&2
-            ${pkgs.mkalias}/bin/mkalias "$src" "/Applications/Nix Apps/$app_name"
-          done
-        '';
+        system.activationScripts.applications.text =
+          let
+            env = pkgs.buildEnv {
+              name = "system-applications";
+              paths = config.environment.systemPackages;
+              pathsToLink = "/Applications";
+            };
+          in
+          pkgs.lib.mkForce ''
+            # Set up applications.
+            echo "setting up /Applications..." >&2
+            rm -rf /Applications/Nix\ Apps
+            mkdir -p /Applications/Nix\ Apps
+            find ${env}/Applications -maxdepth 1 -type l -exec readlink '{}' + |
+            while read -r src; do
+              app_name=$(basename "$src")
+              echo "Making alias $src" >&2
+              ${pkgs.mkalias}/bin/mkalias "$src" "/Applications/Nix Apps/$app_name"
+            done
+          '';
 
-      # Necessary for using flakes on this system.
-      nix.settings.experimental-features = "nix-command flakes";
+        # Necessary for using flakes on this system.
+        nix.settings.experimental-features = "nix-command flakes";
 
-      # Enable alternative shell support in nix-darwin.
-      programs.zsh.enable = true;
-      # programs.fish.enable = true;
+        # Enable alternative shell support in nix-darwin.
+        programs.zsh.enable = true;
+        # programs.fish.enable = true;
 
-      # Set Git commit hash for darwin-version.
-      system.configurationRevision = self.rev or self.dirtyRev or null;
+        # Set Git commit hash for darwin-version.
+        system.configurationRevision = self.rev or self.dirtyRev or null;
 
-      # Used for backwards compatibility, please read the changelog before changing.
-      # $ darwin-rebuild changelog
-      system.stateVersion = 5;
+        # Used for backwards compatibility, please read the changelog before changing.
+        # $ darwin-rebuild changelog
+        system.stateVersion = 5;
 
-      # The platform the configuration will be used on.
-      nixpkgs.hostPlatform = "aarch64-darwin";
+        # The platform the configuration will be used on.
+        nixpkgs.hostPlatform = "aarch64-darwin";
+      };
+    in
+    {
+      # Build darwin flake using:
+      # $ darwin-rebuild build --flake .#Terrences-Virtual-Machine
+      darwinConfigurations."baseline" = nix-darwin.lib.darwinSystem {
+        modules = [
+          configuration
+          nix-homebrew.darwinModules.nix-homebrew
+          {
+            nix-homebrew = {
+              # Install Homebrew under the default prefix
+              enable = true;
+
+              # Apple Silicon Only: Also install Homebrew under the default Intel prefix for Rosetta 2
+              # enableRosetta = true;
+
+              # User owning the Homebrew prefix
+              user = "terrence";
+
+              autoMigrate = true;
+
+              # Optional: Declarative tap management
+              # taps = {
+              #   "homebrew/homebrew-core" = homebrew-core;
+              #   "homebrew/homebrew-cask" = homebrew-cask;
+              #   "homebrew/homebrew-bundle" = homebrew-bundle;
+              # };
+
+              # Optional: Enable fully-declarative tap management
+              #
+              # With mutableTaps disabled, taps can no longer be added imperatively with `brew tap`.
+              # mutableTaps = false;
+            };
+          }
+        ];
+      };
     };
-  in
-  {
-    # Build darwin flake using:
-    # $ darwin-rebuild build --flake .#Terrences-Virtual-Machine
-    darwinConfigurations."baseline" = nix-darwin.lib.darwinSystem {
-      modules = [
-        configuration
-        nix-homebrew.darwinModules.nix-homebrew
-        {
-          nix-homebrew = {
-            # Install Homebrew under the default prefix
-            enable = true;
-
-            # Apple Silicon Only: Also install Homebrew under the default Intel prefix for Rosetta 2
-            # enableRosetta = true;
-
-            # User owning the Homebrew prefix
-            user = "terrence";
-
-            autoMigrate = true;
-
-            # Optional: Declarative tap management
-            # taps = {
-            #   "homebrew/homebrew-core" = homebrew-core;
-            #   "homebrew/homebrew-cask" = homebrew-cask;
-            #   "homebrew/homebrew-bundle" = homebrew-bundle;
-            # };
-
-            # Optional: Enable fully-declarative tap management
-            #
-            # With mutableTaps disabled, taps can no longer be added imperatively with `brew tap`.
-            # mutableTaps = false;
-          };
-        }
-      ];
-    };
-  };
 }
